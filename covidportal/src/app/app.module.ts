1819d36183c8e0ba8fb1563a95304ed3ffd09383
albynbabu <albynbabu97@gmail.com>
2022-02-11 00:35:43 +0530

    initial commit

--- covidportal/src/app/app.module.ts
+++ covidportal/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ApplicationComponent } from './application/application.component';
+import { DashboardComponent } from './application/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ApplicationComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
